c184b379965b0d426abc575078f904dfdff191c4
주현성 <joo@juhyeonseong-ui-MacBookPro.local>
2024-12-02 21:09:08 +0900

    241202-2

--- 1st_week/01_06_find_prime_list_under_number.py
+++ 1st_week/01_06_find_prime_list_under_number.py
@@ -3,7 +3,14 @@ input = 20
 
 def find_prime_list_under_number(number):
     # 이 부분을 채워보세요!
-    return []
+    prime_list = []
+    for i in range(2, number+1):
+        for n in (2, i):
+            if i % n == 0:
+                break
+            else:
+                prime_list.append(i)
+    return prime_list
 
 
 result = find_prime_list_under_number(input)
